618dc8c878410d17868859463d39fab843612eca
Danil <popovlfybk@yandex.ru>
2025-07-12 15:44:19 +0300

    Merged

--- backend/models/chats.go
+++ backend/models/chats.go
@@ -8,6 +8,7 @@ import (
 type Chat struct {
 	ID       uint `gorm:"primaryKey"`
 	UserID   uint
+	Title    string
 	Messages []Message `gorm:"one2many:messagess;"`
 }
 
@@ -15,9 +16,18 @@ type ChatGorm struct {
 	db *gorm.DB
 }
 
-func (cg *ChatGorm) CreateChat(userID int) error {
+func (cg *ChatGorm) CreateChat(userID int, title string) error {
 	chat := &Chat{
 		UserID: uint(userID),
+		Title:  title,
 	}
-	return cg.db.Create(chat).Error
+	return cg.db.Table("chats").Create(chat).Error
+}
+
+func (cg *ChatGorm) GetChat(chatID int) (*Chat, error) {
+	var chat Chat
+	if err := cg.db.Table("chats").Where("id = ?", chatID).Scan(&chat).Error; err != nil {
+		return nil, err
+	}
+	return &chat, nil
 }
--- backend/models/drugs.go
+++ backend/models/drugs.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Drug struct {
 	ID          uint `gorm:"primaryKey"`
@@ -8,4 +12,40 @@ type Drug struct {
 	Description string
 	Expiry      time.Time
 	Location    string
+}
+
+type DrugGorm struct {
+	DB *gorm.DB
+}
+
+func (dg *DrugGorm) CreateDrug(args map[string]interface{}) (*Drug, error) {
+
+	drug := &Drug{}
+
+	if val, ok := args["Type"].(string); ok {
+		drug.Type = val
+	}
+	if val, ok := args["Description"].(string); ok {
+		drug.Description = val
+	}
+	if val, ok := args["Expiry"].(time.Time); ok {
+		drug.Expiry = val
+	}
+	if val, ok := args["Location"].(string); ok {
+		drug.Location = val
+	}
+
+	if err := dg.DB.Table("drugs").Create(drug).Error; err != nil {
+		return nil, err
+	}
+	return drug, nil
+
+}
+
+func (dg *DrugGorm) GetDrug(id int) (*Drug, error) {
+	var drug Drug
+	if err := dg.DB.Table("drugs").Where("id = ?", id).Scan(&drug).Error; err != nil {
+		return nil, err
+	}
+	return &drug, nil
 }
