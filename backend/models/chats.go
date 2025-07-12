a004938218917950eebb05608ff93ed612d810b7
Danil <popovlfybk@yandex.ru>
2025-07-12 12:41:53 +0300

    Add db usage example

--- backend/models/chats.go
+++ backend/models/chats.go
@@ -1,7 +1,23 @@
 package models
 
+import (
+	_ "gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
 type Chat struct {
 	ID       uint `gorm:"primaryKey"`
 	UserID   uint
 	Messages []Message `gorm:"one2many:messagess;"`
+}
+
+type ChatGorm struct {
+	db *gorm.DB
+}
+
+func (cg *ChatGorm) CreateChat(userID int) error {
+	chat := &Chat{
+		UserID: uint(userID),
+	}
+	return cg.db.Create(chat).Error
 }
