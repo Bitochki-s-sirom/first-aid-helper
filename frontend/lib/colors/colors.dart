7d3ce46714fc7d2cf96c38694a9cb051122cadfa
shanme <vadim.poponnikov@yandex.ru>
2025-07-12 12:54:05 +0300

    Created a basic outline of the site with implemented dark and light themes

---
+++ frontend/lib/colors/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const Color kSidebarColor = Color(0xFFAFC7BD);
+
+const Color kSidebarActiveColor = Color(0xFF7D9887);
+
+const Color kSidebarIconColor = Colors.white70;
+
+const Color kDarkSidebarIconColor = Color.fromARGB(179, 0, 0, 0);
+
+const Color kBackgroundColor = Color(0xFFF4F7F6);
+
+const Color kDarkBackgroundColor = Color.fromARGB(255, 39, 38, 38);
