04c7fb8381c18ae4aa8f03d7454d836f40ab0f27
arseniy <ar.boiko@innopolis.university>
2025-07-12 16:01:33 +0300

    parts of CRUD for groups and drugs

--- backend/models/drugs.go
+++ backend/models/drugs.go
@@ -42,10 +42,35 @@ func (dg *DrugGorm) CreateDrug(args map[string]interface{}) (*Drug, error) {
 
 }
 
-func (dg *DrugGorm) GetDrug(id int) (*Drug, error) {
+func (dg *DrugGorm) GetDrugById(id int) (*Drug, error) {
 	var drug Drug
 	if err := dg.DB.Table("drugs").Where("id = ?", id).Scan(&drug).Error; err != nil {
 		return nil, err
 	}
 	return &drug, nil
+}
+
+func (dg *DrugGorm) UpdateDrug(id int, args map[string]interface{}) (*Drug, error) {
+	drug, err := dg.GetDrugById(id)
+	if err != nil {
+		return nil, err
+	}
+
+	if val, ok := args["Type"].(string); ok {
+		drug.Type = val
+	}
+	if val, ok := args["Description"].(string); ok {
+		drug.Description = val
+	}
+	if val, ok := args["Expiry"].(time.Time); ok {
+		drug.Expiry = val
+	}
+	if val, ok := args["Location"].(string); ok {
+		drug.Location = val
+	}
+
+	if err := dg.DB.Table("drugs").Save(drug).Error; err != nil {
+		return nil, err
+	}
+	return drug, nil
 }
